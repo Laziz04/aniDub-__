8ad09e48618098401088128298029c20d3caee99
Asilbek <asilbekargumboyev11@mail.com>
2024-08-07 13:10:03 +0500

    .

--- src/Pages/animeNews/index.tsx
+++ src/Pages/animeNews/index.tsx
@@ -2,8 +2,13 @@ const News = () => {
   return (
     <div>
       <h1>anime News</h1>
+      {/* jhdsdflkjkhjgh */}
+      
     </div>
   );
 };
 
 export default News;
+
+
+// https://s3-alpha-sig.figma.com/img/b323/90e5/6a6ed29982732fc28426edc2f3927d03?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=f1jDNAgiQjpcrDQZAWvaggYW3EETyPDaoIkQIMl~VnGOqX9Smeu0KpMlJmWBNetYUP2-gfY8u7UkkHgkzNrefLvUHIgGnWSyzGTSmM34QwDAyeqGcRRRZclriTRKMBVpZwbqKbjnVeKK5PquYXQL-~Bb-Nf5laoD1wenB8iwoumjyX9A-R53VJo3DsBx4oofOdzz-Vx2TJToR1AMPA7aJw2qIZx9j-gZO2~B4~0reB4I1M9spMBMqCACmLhVwEtiWKOIgi9kbczy27xJEkIzYD~WwBjy6U48DzaqpY-Ir4SPuUW~wGREOSofP0JB70YMra7OH-zRN6C~MyVCJjhv6A__
